b817898d647574b5cb1656ec61af820abfa436da
PedroMP <pedro.magdaleno@sedecal.com>
2026-06-29 09:51:53 +0200

    [fix] Enhance fullUrl function to handle file protocol correctly

--- src/io/itk/itkConfig.js
+++ src/io/itk/itkConfig.js
@@ -1,4 +1,11 @@
 const fullUrl = (relative) => {
+  const isFileProtocol = window.location.protocol === 'file:';
+
+  if (isFileProtocol) {
+    const relativePath = relative.replace(/^\/+/, '');
+    return new URL(relativePath, document.baseURI).href;
+  }
+
   const origin = window.location.origin;
   return `${origin}${relative}`;
 };
